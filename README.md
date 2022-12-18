e8044d60ed1680a3f274ad24cae414d41291b172
Yasser Naji <yasser@Yassers-Air.broadband>
2022-12-18 01:00:11 +0000

    Initialise xLog

--- README.md
+++ README.md
@@ -1,2 +1,209 @@
 # xLog
-A Python package to simplify 
+xLog allows you to output logs in your console or terminal with a little _zing_.
+This package provides an easy-to-use interface to <span style="color:red">colorize</span>, _italicize_, **bolden**, <span style="text-decoration: underline">underline</span> (and much more!) your python logs by simply setting exactly what you need in an xLog class.
+
+This documentation will highlight how you can do this in the sections below - but if you are too excited to wait, you can clone the package now and run `python3 xlog.py --help` which will provide a helpful interactive interface which allows you to navigate to sections you would like to know more about.
+
+<h1>Contents</h1>
+
+1. [fg (foreground)](#fg)
+    * [Escape Codes](#fg-escape)
+    * [RGB](#fg-rgb)
+    * [8-bit Colors](#fg-8bit)
+2. [bg (background)](#bg)
+    * [Escape Codes](#bg-escape)
+    * [RGB](#bg-rgb)
+    * [8-bit Colors](#bg-8bit)
+3. [styles](#styles)
+4. [func](#func)
+    * [Output](#output)
+    * [Errors](#errors)
+5. [Return a string](#return-string)
+
+
+<h1 id="fg">fg (foreground)</h1>
+The fg parameter allows you to set the color of the text. There are several ways to do this, elaborated below:
+
+
+<h2 id="fg-escape">Escape Codes</h2>
+
+Here is the table of escape codes available:
+
+| Name | Escape Code | Name | Escape Code |
+| ----------- | ----------- | ----------- | ----------- |
+| <span style="color: rgb(1,1,1); background-color: white">black </span> | 30 | <span style="color: rgb(128,128,128)">black_hi</span> | 90 |
+| <span style="color: rgb(222,56,43)">red</span> | 31 | <span style="color: rgb(255,0,0)">red_hi</span> | 91 |
+| <span style="color: rgb(57,181,74)">green</span> | 32 | <span style="color: rgb(0,255,0)">green_hi</span> | 92 |
+| <span style="color: rgb(255,199,6)">yellow</span> | 33 | <span style="color: rgb(255,255,6)">yellow_hi</span> | 93 |
+| <span style="color: rgb(0,111,184)">blue</span> | 34 | <span style="color: rgb(0,0,255)">blue_hi</span> | 94 | 
+| <span style="color: rgb(118,38,113)">magenta</span> | 35 | <span style="color: rgb(255,0,255)">magenta_hi</span> | 95 | 
+| <span style="color: rgb(44,181,233)">cyan</span> | 36 | <span style="color: rgb(0,255,255)">cyan_hi</span> | 96 | 
+| <span style="color: rgb(204,204,204)">white</span> | 37 | <span style="color: rgb(255,255,255); background-color: black">white_hi</span> | 97 |
+
+To implement any of these colors in your prompt, you can set `fg` to the name of the color (on the left) **_or_** the escape code (on the right).
+
+For example:
+
+```
+from xlog import xLog
+
+prompt = xLog(fg="red") # Or you can set fg=31
+prompt("I am in red!")
+```
+
+> \> <span style="color: rgb(222,56,43)">I am in red!</span>
+
+<h2 id="fg-rgb">RGB</h2>
+
+To use the RGB color scheme, you can pass a tuple of length 3 with RGB values.
+
+```
+from xlog import xLog
+
+prompt = xLog(fg=(31, 165, 161)) # A kind of teal color
+prompt("I was made by using RGB colors!")
+```
+
+> \> <span style="color: rgb(31,165,161)">I was made by using RGB colors!</span>
+
+<h2 id="fg-8bit">8-bit Colors</h2>
+Finally, we can use 8-bit colors, defined in a range from 0 to 255:
+
+<img src="8bit.png" width="600"/>
+
+**_NOTE_**: When using an 8-bit color for the foreground, you must use the `fg_255` parameter (as opposed to `fg`). If you set `fg` at the same time, `fg_255` will take precedence. 
+
+Example with 8-bit coloring:
+
+```
+from xlog import xLog
+
+prompt = xLog(fg_255=214)
+prompt("I would call this color pumpkin!")
+```
+> \> <span style="color: rgb(255,175,0)">I would call this color pumpkin!</span>
+
+<h1 id="bg">bg (background)</h1>
+The bg parameter will set the background color of the string you pass in the xLog object.
+
+
+<h2 id="bg-escape">Background Escape Codes</h2>
+Your background color options are:
+
+| Name | Escape Code | Name | Escape Code |
+| ----------- | ----------- | ----------- | ----------- |
+| <span style="background-color: rgb(1,1,1); color: white">black</span> | 40 | <span style="background-color: rgb(128,128,128)">black_hi</span> | 100 |
+| <span style="background-color: rgb(222,56,43)">red</span> | 41 | <span style="background-color: rgb(255,0,0)">red_hi</span> | 101 |
+| <span style="background-color: rgb(57,181,74); color: black">green</span> | 42 | <span style="background-color: rgb(0,255,0); color: black">green_hi</span> | 102 |
+| <span style="background-color: rgb(255,199,6); color: black">yellow</span> | 43 | <span style="background-color: rgb(255,255,6); color: black">yellow_hi</span> | 103 |
+| <span style="background-color: rgb(0,111,184)">blue</span> | 44 | <span style="background-color: rgb(0,0,255)">blue_hi</span> | 104 | 
+| <span style="background-color: rgb(118,38,113)">magenta</span> | 45 | <span style="background-color: rgb(255,0,255); color: black">magenta_hi</span> | 105 | 
+| <span style="background-color: rgb(44,181,233); color: black">cyan</span> | 46 | <span style="background-color: rgb(0,255,255); color: black">cyan_hi</span> | 106 | 
+| <span style="background-color: rgb(204,204,204); color: black">white</span> | 47 | <span style="background-color: rgb(255,255,255); color: black">white_hi</span> | 107 |
+
+Implementing background escape code colors is almost identical to setting the foreground colors (you just use the bg parameter instead):
+
+```
+from xlog import xLog
+
+prompt = xLog(bg="red_hi") # can equally do `bg=101`
+prompt("I am red, red is me")
+```
+> \> <span style="background-color: rgb(255,0,0)">I am red, red is me</span>
+
+<h2 id="bg-rgb">RGB</h2>
+Using the RGB color scheme for background colors is identical to that of the foreground colors, with the only difference being that the `bg` parameter should be used instead:
+
+```
+from xlog import xLog
+
+prompt = xLog(bg=(135,0,255))
+prompt("A cool d color!")
+```
+> \> <span style="background-color: rgb(135,0,255)">A cool background color!</span>
+
+<h1 id="styles">styles</h1>
+
+The style parameter allows you to print various font styles such as **bold**, *italic*, <span style="text-decoration: underline">underline</span> etc.
+
+Your escape code style options are:
+
+| name | Description |
+| ----------- | ----------- |
+| **bold** | 1 |
+| <span style="color: rgb(79,79,79)">faint</span> | 2 |
+| _italic_ | 3 |
+| <span style="text-decoration: underline">underline</span> | 4 |
+| <img src="blinking.gif" width="65"/> | 5 |
+| ~~strikethrough~~ | 9 |
+
+<span style="font-size:10px">Note: this table may be expanded if necessary or upon request</span>
+
+You can implement one style or several styles simultaneously.
+
+We can set one particular style in the following way:
+```
+from xlog import xLog
+
+prompt_1 = xLog(style=1) # you can also set style="bold"
+prompt_1("Look, i'm bold!")
+```
+> \> **Look, i'm bold!**
+
+You can also utilise several different styles by setting the `styles` parameter to a set, list or tuple containing the styles name or escape codes:
+
+```
+from xlog import xLog
+
+prompt = xLog(style=[1, "italic", 4]) # Yes, you can mix and match different data types!
+prompt("I'm bold, italic and underlined!")
+```
+> \> <span style="text-decoration: underline">**_I'm bold, italic and underlined!_**</span>
+
+<h1 id="func">func</h1>
+
+<h2 id="output">Output</h2>
+You can specify which function you want to use to output your prompts. The built-in `print` function is used by default.
+
+For example:
+
+```
+from xlog import xLog
+import logging
+
+prompt = xLog(fg="green", func=logging.warning)
+prompt("I am the Grinch!")
+```
+> \> WARNING:root:<span style="color: rgb(57,181,74)">I am the Grinch!</span>
+
+<h2 id="raising-errors">Raising Errors</h2>
+You can also use `xLog` to raise errors by setting `func` as an exception class:
+
+```
+class CustomError(Exception):
+    pass
+
+err_prompt = xLog(bg=101, func=CustomError)
+err_prompt("This custom error has been raised!")
+```
+
+> \> \_\_main\_\_.CustomError: <span style="background-color: rgb(255,0,0)">This custom error has been raised!</span>
+
+<h2 id="return-str">Returning a string</h2>
+
+If you do not feel like printing your (cooler looking) texts just yet, you have the option to save the string (along with the colors and styles) to be printed later if you wish.
+
+This is done via the `return_str` method:
+
+```
+from xlog import xLog
+
+prompt = xLog(fg="red_hi", bg=44, styles = [1, "italic"])
+xlog_str = prompt.return_str("I will be printed later, and it will be great!")
+
+# Do whatever you like in-between
+
+print(xlog_str)
+```
+
+> \> <span style="background-color: rgb(0,0,255); color: rgb(255,0,0)">**_I will be printed later, and it will be great!_**</span>
---
+++ xlog.py
@@ -0,0 +1,396 @@
+import sys
+from enum import Enum
+
+
+class EscapeCodes(Enum):
+    reset = 0
+    # Text Styles
+    bold = 1
+    dim = 2
+    italic = 3
+    underline = 4
+    blinking = 5
+    strikethrough = 9
+    # Text Colours
+    black = 30
+    red = 31
+    green = 32
+    yellow = 33
+    blue = 34
+    purple = 35
+    cyan = 36
+    white = 37
+    # Background Colours
+    black_bg = 40
+    red_bg = 41
+    green_bg = 42
+    yellow_bg = 43
+    blue_bg = 44
+    purple_bg = 45
+    cyan_bg = 46
+    white_bg = 47
+    # High Intensity Text Colours
+    black_hi = 90
+    red_hi = 91
+    green_hi = 92
+    yellow_hi = 93
+    blue_hi = 94
+    purple_hi = 95
+    cyan_hi = 96
+    white_hi = 97
+    # High Intensity Background Colours
+    black_hi_bg = 100
+    red_hi_bg = 101
+    green_hi_bg = 102
+    yellow_hi_bg = 103
+    blue_hi_bg = 104
+    purple_hi_bg = 105
+    cyan_hi_bg = 106
+    white_hi_bg = 107
+
+
+class xLogError(Exception):
+    __module__ = "builtins"
+
+
+class xLog:
+    def __init__(
+        self, bg=None, fg=None, styles=[], func=print, fg_255=None, bg_255=None
+    ):
+        self.func = func
+        self.config = []
+
+        if not any((fg, bg, styles,)) or (fg_255 is not None or bg_255 is not None):
+            print(
+                "xLog: No configurations specified - "
+                "will just use terminal default (unless specified elsewhere)"
+            )
+            self.config = ""
+            return
+
+        def rgb_validator(rgb):
+            assert len(rgb) == 3 and all(
+                (-1 < int(rgb[0]), int(rgb[1]), int(rgb[2]) < 256)
+            )
+
+        def is_int_or_int_str(code):
+            return type(code) is int or (type(code) is str and code.isdigit())
+
+        def str_to_escape_code(code, context="fg"):
+
+            code = code.lower()
+            hi = 0
+            bg = ""
+
+            if code[-2:] == "hi":
+                hi = 60
+                code = code[:-3]
+            if context == "bg":
+                bg = "_bg"
+
+            return EscapeCodes[code + bg].value + hi
+
+        def escape_code_validator(code, context):
+            fg = 29 < code < 38 or 89 < code < 98
+            bg = 39 < code < 48 or 98 < code < 108
+
+            if context == "fg":
+                assert fg
+            elif context == "bg":
+                assert bg
+            elif context == "style":
+                assert not fg and not bg
+
+        def set_color(var, var_255, _fg=False):
+            context = context = "fg" if _fg else "bg"
+            try:
+                if _fg:
+                    esc_code = "38"
+                else:
+                    esc_code = "48"
+
+                if var_255:
+                    var = None
+                    assert -1 < var_255 < 256
+                    self.config += [esc_code, "5", str(var_255)]
+                elif is_int_or_int_str(var):
+                    escape_code_validator(int(var), context)
+                    self.config.append(str(var))
+                elif type(var) is str:
+                    code = str_to_escape_code(var, context)
+                    escape_code_validator(code, context)
+                    self.config.append(str(code))
+                elif type(var) is tuple:
+                    rgb_validator(var)
+                    self.config += [
+                        esc_code,
+                        "2",
+                        str(var[0]),
+                        str(var[1]),
+                        str(var[2]),
+                    ]
+                else:
+                    err = f"{var if var else var_255} "
+                    err += "is using an incompatible datatype for "
+                    err += f"{context}\n"
+                    err += f"{context} must be either int, str or tuple\n"
+                    err += "Refer to xLog().help() "
+                    err += "if you need further guidance"
+                    raise xLogError(err)
+            except AssertionError:
+                if var_255:
+                    err = f"{var_255} is not a valid code for {context}_255\n"
+                    err += "Use int between (inclusive) 0 and 255"
+                else:
+                    err = f"{var} is not a valid code for {context}\n"
+                    err += "Refer to xLog().help() "
+                    err += "if you need further guidance"
+                raise xLogError(err)
+            except TypeError:
+                if var_255:
+                    err = f"Incorrect datatype used for {context}_255\n"
+                    err += "Use int between (inclusive) 0 and 255"
+                else:
+                    err = f"{var} is not a valid code for {context}\n"
+                    err += "Refer to xLog().help() "
+                    err += "if you need further guidance"
+                raise xLogError(err)
+            except KeyError:
+                if is_int_or_int_str(var):
+                    err = f"Unkown Escape code {var}"
+                elif type(var) is str:
+                    err = f"Unknown Escape code string value <{var}>\n"
+                err += "Refer to xLog().help() "
+                err += "if you need further guidance"
+                raise xLogError(err)
+
+        colors = [
+            (var, var_255, _fg)
+            for var, var_255, _fg in [(fg, fg_255, True), (bg, bg_255, False)]
+            if any((var, var_255))
+        ]
+
+        for color, color_255, _fg in colors:
+            set_color(color, color_255, _fg)
+
+        if type(styles) in (str, int):
+            styles = [styles]
+        elif not hasattr(styles, "__iter__"):
+            err = f"Datatype {type(styles)} for styles is incompatible\n"
+            err += "Please provide either int, str or an iterable"
+            raise xLogError(err)
+
+        for style in styles:
+            try:
+                if is_int_or_int_str(style):
+                    escape_code_validator(int(style), "style")
+                    self.config.append(str(EscapeCodes(int(style)).value))
+                else:
+                    style = int(str_to_escape_code(style))
+                    escape_code_validator(style, "style")
+                    self.config.append(str(style))
+            except KeyError:
+                if is_int_or_int_str(style):
+                    err = f"Unkown code input <{style}>"
+                else:
+                    err = f"Unknown string input <{style}>"
+                raise xLogError(f"{err}")
+            except AssertionError:
+                name = EscapeCodes(style).name
+                name = name.replace('_hi', '').replace('_bg', '')
+                err = f"Cannot set style to {style} ({name})"
+                err += f"{style} is reserved for the "
+                if 29 < int(style) < 38 or 89 < int(style) < 98:
+                    err += "fg parameter"
+                else:
+                    err += "bg parameter"
+                raise xLogError(err)
+
+        self.config = ";".join(self.config)
+
+    def __call__(self, text):
+        text = f"\x1b[{self.config}m{text}\x1b[0m"
+        if type(self.func) is type and issubclass(self.func, Exception):
+            raise self.func(text)
+        self.func(text)
+
+    def return_str(self, text):
+        return f"\x1b[{self.config}m{text}\x1b[0m"
+
+    @staticmethod
+    def help():
+
+        base_config = "\x1b[1;3;40;97"
+        border = "~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~+~\n\n"
+        intro = f"{base_config};4mHello and thank you for using xLog!\n\x1b[0m"
+
+        print(intro)
+
+        def color_prompt(context):
+            bg_shift = 0
+
+            colors = [
+                "black",
+                "red",
+                "green",
+                "yellow",
+                "blue",
+                "purple",
+                "cyan",
+                "white",
+            ]
+
+            colors = (color for color in colors)
+
+            if context == "bg":
+                bg_shift = 10
+
+            msg = ""
+            msg_hi = ""
+            for code in range(30 + bg_shift, 38 + bg_shift):
+                color = next(colors)
+                if code == 30:
+                    msg += f" {base_config};{code};{107}m{color}: {code}\x1b[0m{base_config}m\n"
+                    msg_hi += f" {base_config};{code+60};{107}m{color}_hi: {code+60}\x1b[0m{base_config}m\n"
+                elif code == 47:
+                    msg += f" {base_config};{code};{90}m{color}: {code}\x1b[0m{base_config}m\n"
+                    msg_hi += f" {base_config};{code+60};{90}m{color}_hi: {code+60}\x1b[0m{base_config}m\n"
+                else:
+                    msg += (
+                        f" {base_config};{code}m{color}: {code}\x1b[0m{base_config}m\n"
+                    )
+                    msg_hi += f" {base_config};{code+60}m{color}_hi: {code+60}\x1b[0m{base_config}m\n"
+
+            hi = 'You also have a choice of "High Intensity" (hi) colors:\n'
+
+            return msg + "\n\n" + hi + msg_hi + "\n"
+
+        option = f"{base_config}mChoose one of the following options for details on options and implementation:\n\n"
+        option += "-" + f"{'foreground (fg)': >20}\n"
+        option += "-" + f"{'background (bg)': >20}\n"
+        option += "-" + f"{'styles (st)': >20}\n"
+        option += "-" + f"{'rgb (rgb)': >20}\n"
+        option += "-" + f"{'255-color (255)': >20}\n"
+        option += "-" + f"{'func (f)': >20}\n\n"
+        option += "** NOTE: Some styles and functionalities may not work on certain terminals! **"
+        option += "\n\nor alternatively, you can quit (q)\n\n"
+
+        while True:
+            go_back = False
+
+            choice = input(option)
+            choice = choice.lower()
+
+            if choice in ("fg", "foreground"):
+                msg = "\n\n" + border
+                msg += "Here are the foreground color options:\n"
+                msg += color_prompt("fg")
+                msg += "You can set the foreground color setting the fg parameter in the following ways:\n\n"
+                msg += 'prompt = xLog(fg="red") ~OR~ prompt = xLog(fg=31)\n\n'
+                msg += 'prompt("Hello World!")\n'
+                msg += f"> {base_config};31mHello World!{base_config}m\n"
+                msg += 'You can do the same with "high intensity" (hi) colors like this:\n\n'
+                msg += (
+                    'prompt = xLog(fg="red_hi") # append color name with _hi\n'
+                )
+                msg += 'prompt("Hello World!")\n'
+                msg += f"> {base_config};91mHello World!{base_config}m\n\n"
+                msg += border
+                print(msg)
+
+            elif choice in ("bg", "background"):
+                msg = "\n\n" + border
+                msg += "Here are the background color options:\n"
+                msg += color_prompt("bg")
+                msg += "You can set the foreground color setting the bg parameter in the following ways:\n\n"
+                msg += (
+                    'prompt = xLog(bg="red") ~OR~ prompt = xLog(bg=41)\n'
+                )
+                msg += 'prompt("Hello World!")\n'
+                msg += f"> {base_config};41mHello World!{base_config}m\n\n"
+                msg += 'You can do the same with "high intensity" (hi) colors like this:\n\n'
+                msg += (
+                    'prompt = xLog(fg="red_hi") # append color name with _hi\n'
+                )
+                msg += 'prompt("Hello World!")\n'
+                msg += f"> {base_config};101mHello World!{base_config}m\n\n"
+                msg += border
+                print(msg)
+
+            elif choice in ("st", "styles"):
+                msg = "\n\n" + border
+                msg += "Here are your style options:\n\n"
+                msg += f" \x1b[40;97;1mbold: 1\x1b[0m\n"
+                msg += f" \x1b[40;2mdim: 2\x1b[0m\n"
+                msg += f" \x1b[40;97;3mitalic: 3\x1b[0m\n"
+                msg += f" \x1b[40;97;4munderline: 4\x1b[0m\n"
+                msg += f" \x1b[40;97;5mblinking: 5\x1b[0m\n"
+                msg += f" \x1b[40;97;9mstrikethrough: 9\x1b[0\n\n"
+
+                msg += "You can one choose particular font style by setting the style parameter in the following ways\n\n"
+                msg += 'prompt = xLog(style="underline") ~OR~ prompt = xLog(style=4)\n'
+                msg += 'prompt("Hello World!")\n'
+                msg += f"> {base_config};4mHello World!\x1b[0m{base_config}m\n\n"
+                msg += "But you can also pass in several style fonts by using a list (or any iterable):\n\n"
+                msg += 'prompt(style=["underline", "strikethrough"]) ~OR~ prompt(style=[4, 9]) ~OR EVEN~ prompt(style=[4, "strikethrough"])\n'
+                msg += f"> {base_config};4;9mHello World!\x1b[0m{base_config}m\n\n"
+                msg += border
+                print(msg)
+
+            elif choice == "rgb":
+                msg = "\n\n" + border
+                msg += "You can use RGB values setting the fg and bg parameters to tuples of the form (r, g, b) \n"
+                msg += "where r, g and b are the rgb values between 0 and 255 (inclusive)\n\n"
+                msg += "prompt = xLog(fg=(255,0,0), bg=(0,0,255))\n"
+                msg += 'prompt("Hello World!")\n'
+                msg += (
+                    f"> \x1b[38;2;255;0;0;48;2;0;0;255mHello World!{base_config}m\n\n"
+                )
+                msg += border
+                print(msg)
+
+            elif choice == "255":
+                msg = "\n\n" + border
+                msg += "You need to use the fg_255 (foreground) and bg_255 (background) parameters for 255-color palette\n\n"
+                msg += "prompt = xLog(fg_255=196, bg_255=21)\n"
+                msg += 'prompt("Hello World!")\n'
+                msg += f"> \x1b[38;5;196;48;5;21mHello World!{base_config}m\n\n"
+                msg += border
+                print(msg)
+            elif choice in ("f", "func"):
+                msg = "\n\n" + border
+                msg += "You can set your own console logging function by using the func parameter\n\n"
+                msg += "import logging\n"
+                msg += 'prompt = xLog(fg="red", func=logging.warning)\n'
+                msg += 'prompt("Oh no, something is not quite right!")\n'
+                msg += f"> WARNING:root:{base_config};31mOh no, something is not quite right!\x1b[0m{base_config}m\n\n"
+
+                msg += "You can also raise errors using func!\n\n"
+                msg += "class CustomError(Exception): # You must inherit from Exception for this to work\n"
+                msg += "    pass\n\n"
+                msg += 'prompt = xLog(fg="red", func=CustomError)\n'
+                msg += 'prompt("This is a custom error!")\n'
+                msg += "... (traceback)\n"
+                msg += f"> __main__.CustomError: {base_config};31mThis is a custom error!\x1b[0m{base_config}m\n\n"
+                msg += border
+                print(msg)
+            elif choice in ("q", "quit"):
+                break
+            else:
+                go_back = True
+                print("\nSorry, I didn't quite get that\n\n")
+
+            if not go_back:
+                choice = input(
+                    'Press enter to continue or "quit" (q) and enter to exit\n\n'
+                )
+
+            if choice in choice in ("q", "quit"):
+                break
+
+        print("Goodbye and good luck!")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2 and sys.argv[1] in ("help", "--help"):
+        xLog().help()
